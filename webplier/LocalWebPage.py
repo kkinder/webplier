37606d3d0f2ed0409b064fe9874e991dc0f6cc36
Ken Kinder <ken@kkinder.com>
2013-03-07 20:01:28 -0700

    Add user agent.

--- webplier/LocalWebPage.py
+++ webplier/LocalWebPage.py
@@ -1,6 +1,9 @@
 import webbrowser
-from PyQt4.QtWebKit import QWebPage
+from PyQt4.QtCore import QString
+from PyQt4.QtWebKit import QWebPage, qWebKitVersion
 
+BROWSER_USER_AGENT = 'Mozilla/5.0 (Linux) AppleWebKit/534.34 (KHTML, like Gecko) Webplier-browser/1.0' % (
+    qWebKitVersion())
 
 class LocalWebPage(QWebPage):
     def triggerAction(self, action, checked=False):
@@ -9,3 +12,6 @@ class LocalWebPage(QWebPage):
             webbrowser.open(url, 1)
         else:
             return super(LocalWebPage, self).triggerAction(action, checked)
+
+    def userAgentForUrl(self, url):
+        return QString(BROWSER_USER_AGENT)
